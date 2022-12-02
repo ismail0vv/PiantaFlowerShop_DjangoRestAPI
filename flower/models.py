e598b49133b776ec6ab279ca9c923f1819308074
Mapaipov <mataipovmage@gmail.com>
2022-12-02 16:03:37 +0600

    photo + flower model

--- flower/models.py
+++ flower/models.py
@@ -2,14 +2,13 @@ from django.db import models
 from Pianta import settings
 
 
-# Create your models here.
-# Моделька Цветов (flower)
-# title
-# price
-# description
-# categories ManyToMany
-# colors ManyToMany
-# sold_quantity
+class Flower(models.Model):
+    title = models.CharField(max_length=50)
+    price = models.IntegerField()
+    description = models.CharField(max_length=255)
+    categories = models.ManyToManyField('Category')
+    colors = models.ManyToManyField('Color')
+    sold_quantity = models.IntegerField(default=0)
 
 
 class Category(models.Model):
@@ -19,10 +18,12 @@ class Category(models.Model):
 class Color(models.Model):
     title = models.CharField(max_length=30)
 
+
 # Моделька Фото
 # flower Foreign Key related_name
 # image = ImageField
-
+class Photo(models.Model):
+    flower = models.ForeignKey(Flower, on_delete=models.CASCADE, related_name='photos')
 
 class Review(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
