853ea36c5c77e26d22b4fff53d9f2b8b8a9f5165
d21-1 <110811842+d21-1@users.noreply.github.com>
2022-12-03 14:04:35 +0600

    Update settings.py

--- Pianta/settings.py
+++ Pianta/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'flower',
     'users',
-    'rest_framework_simplejwt.token_blacklist'
+    'rest_framework_simplejwt.token_blacklist',
+    'cart'
 ]
 
 MIDDLEWARE = [
