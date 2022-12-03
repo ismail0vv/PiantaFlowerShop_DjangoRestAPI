a49bbe5bef072134149703bcee8148b154acf12f
AbdimalikBegmatov <abdimalik.b@hotmail.com>
2022-12-03 13:24:24 +0600

    flower app serializers

--- flower/models.py
+++ flower/models.py
@@ -10,6 +10,9 @@ class Flower(models.Model):
     colors = models.ManyToManyField('Color')
     sold_quantity = models.IntegerField(default=0)
 
+    def get_photos_list(self):
+        return [photo.image for photo in self.photos.all()]
+
 
 class Category(models.Model):
     title = models.CharField(max_length=30)
@@ -24,6 +27,8 @@ class Color(models.Model):
 # image = ImageField
 class Photo(models.Model):
     flower = models.ForeignKey(Flower, on_delete=models.CASCADE, related_name='photos')
+    image = models.ImageField()
+
 
 class Review(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
--- flower/serializers.py
+++ flower/serializers.py
@@ -1,2 +1,33 @@
+from rest_framework import serializers
+from .models import *
 
-# Flower LIST GET POST PUT PATCH
+
+class ColorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Color
+        fields = "__all__"
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Color
+        fields = '__all__'
+
+
+class FlowerSerializer(serializers.ModelSerializer):
+    colors = ColorSerializer(many=True)
+    categories = CategorySerializer(many=True)
+    photos = serializers.SerializerMethodField(many=True)
+
+    class Meta:
+        model = Flower
+        fields = '__all__'
+
+    def get_photos(self, flower):
+        return flower.get_photos_list()
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = '__all__'
