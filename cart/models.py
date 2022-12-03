f47802e6e74fdd7e349815eb0c0176fd057570be
d21-1 <110811842+d21-1@users.noreply.github.com>
2022-12-03 14:02:30 +0600

    cart model

--- cart/models.py
+++ cart/models.py
@@ -3,9 +3,14 @@ from django.db import models
 # Create your models here.
 # Корзина Cart
 # user пользователь
-
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 # Моделька CartItem
 # cart foreignKey корзина
 # flower цветы берешь модельку ForeignKey
 # quantity количество
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    flower = models.ForeignKey(Flower, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
