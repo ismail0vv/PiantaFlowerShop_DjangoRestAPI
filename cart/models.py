5917e1700212ee5f8f22167f819c08566d9150c5
ismail0vv <asylismailov05@gmail.com>
2022-12-04 13:25:57 +0600

    fixed cart models

--- cart/models.py
+++ cart/models.py
@@ -1,16 +1,14 @@
 from django.db import models
+from Pianta import settings
+from flower.models import Flower
+
 
 # Create your models here.
-# Корзина Cart
-# user пользователь
 class Cart(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
-# Моделька CartItem
-# cart foreignKey корзина
-# flower цветы берешь модельку ForeignKey
-# quantity количество
+
 class CartItem(models.Model):
-    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
     flower = models.ForeignKey(Flower, on_delete=models.CASCADE)
     quantity = models.IntegerField(default=1)
