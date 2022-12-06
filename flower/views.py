007894b158f74b05ff76741e790fac9640bc462e
RazanBek <kubandkovbeknazar@gmail.com>
2022-12-06 08:26:20 +0600

    category client views

--- flower/views.py
+++ flower/views.py
@@ -14,6 +14,21 @@ class FlowerListApiView(generics.ListAPIView):
     serializer_class = FlowerSerializer
 
     def get_queryset(self):
-        queryset = Flower.objects.filter(categories__in=[self.kwargs['cat_id']])
+        queryset = Flower.objects.all()
 
+        return queryset
+
+
+class CategoryListApiView(generics.ListAPIView):
+    permission_classes = [AllowAny]
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
+
+
+class CategoryDetailAPIView(generics.RetrieveAPIView):
+    permission_classes = [AllowAny]
+    serializer_class = CategorySerializer
+
+    def get_queryset(self):
+        queryset = Flower.objects.filter(categories__in=[self.kwargs['pk']])
         return queryset
